f15e94a795af317d4b732c7cc07e4f4fe04e22aa
Ian Charters <ian@boot.rip>
2018-01-25 17:10:54 -0800

    Fixed `dev.secret.exs`

--- config/dev.secret.exs
+++ config/dev.secret.exs
@@ -2,5 +2,5 @@ use Mix.Config
 
 config :proactive_api, ProactiveAPI.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "YOUR_USERNAME_HERE",
-  password: "YOUR_PASSWORD_HERE",
+  username: "USERNAME_HERE",
+  password: "PASSWORD_HERE"
